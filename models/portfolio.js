7f2363892e76ffac4be19492754ebdc37d05db5b
mikla <mikla79@gmail.com>
2018-04-22 17:22:40 +0200

    mdl portfolio ok

--- models/portfolio.js
+++ models/portfolio.js
@@ -2,3 +2,16 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+const portfolioSchema = new Schema({
+  naslov: { type: 'String', required: true },
+  opis: { type: 'String', required: true },
+  cuid: { type: 'String', required: true },
+  slug: { type: 'String', required: true },
+  klijent: { type: 'String', required: true },
+  datProjekta: { type: 'Date', default: Date.now, requred: true },
+  previewSlika: { type: 'String', required: true },
+  slike: ['String'],
+  tagovi: ['String'],
+});
+
+export default mongoose.model('Portfolio', portfolioSchema);
